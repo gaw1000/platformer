a01c307f28b628a29a3223e207c255ac7523c776
gaw1000 <wilson.gaw30@gmail.com>
2016-01-09 14:19:57 +1100

    Final update
    Final working platform

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ var context = canvas.getContext("2d");
 
 var startFrameMillis = Date.now();
 var endFrameMillis = Date.now();
-
 // This function will return the time in seconds since the function 
 // was last called
 // You should only call this function once per frame
@@ -29,12 +28,6 @@ function getDeltaTime()
 
 //-------------------- Don't modify anything above here
 
-var SCREEN_WIDTH = canvas.width;
-var SCREEN_HEIGHT = canvas.height;
-var player = new Player();
-var keyboard = new Keyboard();
-
-
 // some variables to calculate the Frames Per Second (FPS - this tells use
 // how fast our game is running, and allows us to make the game run at a 
 // constant speed)
@@ -42,9 +35,417 @@ var fps = 0;
 var fpsCount = 0;
 var fpsTime = 0;
 
-// load an image to draw
-var chuckNorris = document.createElement("img");
-chuckNorris.src = "hero.png";
+var player;
+var enemy;
+var keyboard = new Keyboard();
+
+var SCREEN_WIDTH = canvas.width;
+var SCREEN_HEIGHT = canvas.height;
+var LAYER_COUNT = 4;
+var LAYER_PLATFORMS = 1;
+var LAYER_LADDERS = 2;
+var LAYER_BACKGROUND = 0;
+var LAYER_ENEMY = 3;
+var LAYER_TRIGGERS = 4;
+
+var LAYERS_MAX_DRAW = 3; //only draw the first 3 layers
+	// the size of the map (in tiles)
+var MAP = { tw: 60, th: 15 };
+var worldOffsetX = 0;
+	// the size of each tile (in game pixels)
+var TILE = 35;
+var TILESET_TILE = TILE * 2;
+var TILESET_PADDING = 2;
+var TILESET_SPACING = 2;
+var TILESET_COUNT_X = 14;
+var TILESET_COUNT_Y = 14;
+
+// add force variables
+var METER = TILE; // arbitary choice for 1 m
+var GRAVITY = METER * 9.8 * 6; // exaggerated gravity by 6
+var MAXDX = METER * 10; //maximum horz. speed 10 tiles per sec
+var MAXDY = METER * 15; // max. vert. speed 15 tiles/sec
+var ACCEL = MAXDX * 2; // hor. accel takes 0.5 sec to reach MAXDX.
+var FRICTION = MAXDX * 6; //takes 1/6 sec to sto[ from maxdx
+var JUMP = METER * 15000; // large instantaneous jump impulse
+
+var STATE_PLAY = 0;
+var STATE_GAME_OVER = 1;
+
+var gameState = STATE_PLAY;
+
+//score and lives intial variables
+var score = 0;
+var lives = 3;
+
+// hear image for lives
+var heartImage = document.createElement("img");
+heartImage.src ="heart_3.png";
+
+// variables for enemy
+var ENEMY_MAXDX = METER * 5;
+var ENEMY_ACCEL = ENEMY_MAXDX * 2;
+
+var enemies = [];
+var bullets = [];
+
+
+var cells = []; //array for simplified collision data
+
+	// load the image to use for the level tiles
+var tileset = document.createElement("img");
+tileset.src = "tileset.png";
+
+//music variables
+var musicBackground;
+var sfxFire;
+var isSfxPlaying = false;
+
+function initialize()
+{	
+	musicBackground = new Howl(
+	{
+		urls: ["background.ogg"],
+		loop: true,
+		buffer: true,
+		volue: 0.5
+	});
+	musicBackground.play();
+	
+	sfxFire = new Howl(
+	{
+		urls: ["fireEffect.ogg"],
+		buffer: true,
+		volume: 1,
+		onend: function(){
+			isSfxPlaying = false;
+		}
+	});	
+	
+	// initialize the collision map
+	for(var layerIdx=0; layerIdx<level1.layers.length; layerIdx++)	
+	{
+		cells[layerIdx] = [];
+	
+		var idx = 0;	
+		for(var y=0; y<level1.layers[layerIdx].height; y++) 
+		{
+			cells[layerIdx][y] = [];
+			for(var x=0; x<level1.layers[layerIdx].width; x++) 
+			{				
+				if(level1.layers[layerIdx].data[idx] != 0)
+				{
+						// for each tile we find in the layer data, we need
+						// to create 4 collisions (because our collision squares
+						// are 35x35 but the tile in the level are 70x70)
+					cells[layerIdx][y][x] = 1;	
+					cells[layerIdx][y-1][x] = 1;	
+					cells[layerIdx][y-1][x+1] = 1;	
+					cells[layerIdx][y][x+1] = 1;	
+				}				
+				else if(cells[layerIdx][y][x] != 1) 
+				{
+					// if we haven't already set this cell's value (in the above if statement)
+					// then set it to 0 now
+					cells[layerIdx][y][x] = 0;
+				}
+				idx++;
+			}
+		}	
+	}
+	
+	// add enemies
+
+	idx = 0;
+	for(var y=0; y<level1.layers[LAYER_ENEMY].height; y++) 
+	{
+		for(var x=0; x<level1.layers[LAYER_ENEMY].width; x++) 
+		{				
+			if(level1.layers[LAYER_ENEMY].data[idx] != 0)
+			{
+				// create a new enemy and place on this tile
+				var enemy = new Enemy(x * TILE, y * TILE)
+				enemies.push(enemy);
+			}
+			idx++;
+		}
+
+	}	
+
+	player = new Player();
+	
+	
+};
+
+function cellAtPixelCoord(layer, x, y)
+{
+	if(x<0 || x >SCREEN_WIDTH || y<0)
+		return 1;
+	// if player drops off bottom of screen then player dies
+	if(y>SCREEN_HEIGHT)
+		return 0;
+	;return cellAtTileCoord(layer, p2t(x), p2t(y));
+}
+
+function tileToPixel(tile) 
+{
+	return tile * TILE;
+};
+
+function pixelToTile(pixel) 
+{
+	return Math.floor(pixel/TILE); 
+};
+
+function cellAtTileCoord(layer, tx, ty) 
+{ 
+	if(tx<0 || tx>=MAP.tw || ty<0)
+		return 1;
+	// let the player drop of the bottom of the screen (this means death)
+	if(ty>=MAP.th)
+		return 0;		
+	return cells[layer][ty][tx]; 
+};
+
+function bound(value, min, max)
+{
+	if(value < min)
+		return min;
+	if(value > max)
+		return max;
+	return value;
+}
+
+function intersects(x1, y1, w1, h1, x2, y2, w2, h2)
+{
+	if(y2 + h2 < y1 ||
+	x2 + w2 < x1 ||
+	x2 > x1 + w1 ||
+	y2 > y1 + h1)
+	{
+		return false;
+	}
+	return true;
+}
+
+
+function drawMap()
+{
+	var startX = -1;
+	var maxTiles = (SCREEN_WIDTH / TILE) + 2;	
+	var tileX = pixelToTile(player.position.x);
+	var offsetX = TILE + (player.position.x - tileToPixel(tileX));
+	
+	startX = tileX - ((SCREEN_WIDTH / TILE) / 2);
+	
+	if(startX < -1) 
+	{
+		startX = -1;
+		offsetX = TILE;
+	}
+	if(startX > MAP.tw - maxTiles)
+	{
+		startX = MAP.tw - maxTiles + 1;
+		offsetX = TILE;
+	}		
+	
+	worldOffsetX = startX * TILE + offsetX;
+
+	for(var layerIdx=0; layerIdx<LAYERS_MAX_DRAW; layerIdx++)
+	{		
+		for(var y=0; y<level1.layers[layerIdx].height; y++) 
+		{	
+			var idx = y * level1.layers[layerIdx].width + startX;
+				
+			for(var x = startX; x < startX+maxTiles; x++) 
+			{		
+				if(level1.layers[layerIdx].data[idx] != 0)
+				{
+						// the tiles in the Tiled map are base 1 (meaning a value of 0
+						// means no tile), so subtract one from the tileset id to get the
+						// correct tile
+					var tileIndex = level1.layers[layerIdx].data[idx] - 1;
+					var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
+					var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_Y)) * (TILESET_TILE + TILESET_SPACING);
+					
+					context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE, (x-startX)*TILE - offsetX, (y-1)*TILE, TILESET_TILE, TILESET_TILE);
+				}
+				
+				idx++;
+			}
+		}		
+	}
+}
+
+function updateStatePlay(deltaTime)
+{
+	player.update(deltaTime);
+	
+	
+	//update enemy
+	for(var i =0; i<enemies.length; i++)
+	{
+		enemies[i].update(deltaTime);
+		// check if enemy hits player remove one life
+		
+		if(player.isAlive == true)
+		{
+			if( intersects( player.position.x, player.position.y, TILE, TILE,
+						enemies[i].position.x, enemies[i].position.y, TILE, TILE) == true)
+			{
+				player.isAlive = false;
+				lives = lives -1;
+				if(lives > 0)
+				{
+					player.respawn();
+					gameState = STATE_PLAY;
+				}
+				else
+				{
+					gameState = STATE_GAME_OVER;
+				}
+				break;
+			}
+			// Check if player has fallen off the screen
+			if(player.position.y > SCREEN_HEIGHT) // fell off screen
+			{
+				player.isAlive = false;
+				lives = lives -1;
+				if(lives > 0)
+				{
+					player.respawn();
+					gameState = STATE_PLAY;
+				}
+				else
+				{
+					gameState = STATE_GAME_OVER;
+				}
+				break;
+			}
+			// Check if it has reached the end
+			
+			var tx = pixelToTile(player.position.x);
+			var ty = pixelToTile(player.position.y);
+			if(cellAtTileCoord(LAYER_TRIGGERS, tx, ty) !=0)
+			{
+				gameState = STATE_GAME_OVER;
+			}
+			
+		}		
+		
+	}
+	
+	// update bullets
+	var hit = false;
+	for(var i=0; i<bullets.length; i++)
+	{
+		bullets[i].update(deltaTime);
+		// check if bullet has gone off the screen
+		if(bullets[i].position.x - worldOffsetX < 0 || bullets[i].position.x - worldOffsetX > SCREEN_WIDTH)
+		{
+			hit = true;
+		}
+		// check if bullet has hit the enemy
+		for(var j=0; j<enemies.length; j++)
+		{
+			if(intersects(bullets[i].position.x, bullets[i].position.y, TILE, TILE,
+			    enemies[j].position.x, enemies[j].position.y, TILE, TILE))
+			{
+				    // remove the bullet and the enemy
+				    enemies.splice(j, 1);
+				    hit = true;
+				    // increase score
+				    score += 1
+				    break;
+			}
+			    
+		}
+		
+		
+		if(hit==true)
+		{
+			bullets.splice(i, 1);
+			break;
+		}			
+	}		
+}
+
+
+function drawStatePlay()
+{
+	drawMap();
+	player.draw();
+	
+	//draw enemies
+	for(var i =0; i<enemies.length; i++)
+	{
+		enemies[i].draw();
+	}
+	
+	// draw bullets
+	for(var i=0; i<bullets.length; i++)
+	{
+		bullets[i].draw();
+	}
+		
+	//draw the HUD elements
+	
+	//score
+	context.fillStyle = "yellow";
+	context.font = "32px Arial";
+	var scoreText = "Score: " + score;
+	context.fillText(scoreText, SCREEN_WIDTH - 170, 35);
+	
+	// lives counter
+	for(var i=0; i<lives; i++)
+	{
+		context.drawImage(heartImage, 20 + (heartImage.width+2)*i, 10);
+	}	
+}
+
+function updateStateGameOver(deltaTime)
+{
+	player.update(deltaTime);
+	
+	for(var i=0; i<enemies.length; i++)
+	{
+		enemies[i].update(deltaTime);
+	}
+	
+	for(var i=0; i<bullets.length; i++)
+	{
+		bullets[i].update(deltaTime);
+	}
+}
+
+function drawStateGameOver()
+{
+	drawMap();
+	
+	player.draw();
+
+	for(var i=0; i<enemies.length; i++)
+	{
+		enemies[i].draw();
+	}
+	
+	for(var i=0; i<bullets.length; i++)
+	{
+		bullets[i].draw();
+	}
+	
+	// draw score and Game Over
+	context.font = "24px Verdana";
+	context.fillStyle = "#FF0";
+	context.fillText("Score: " + score, 20, 40);
+	
+	context.font = "72px Verdana";
+	context.fillStyle = "#FF0";
+	var width = context.measureText("GAME OVER").width;
+	context.fillText("GAME OVER", SCREEN_WIDTH/2 - width/2, SCREEN_HEIGHT/2)	
+	
+}
+
 
 function run()
 {
@@ -52,12 +453,21 @@ function run()
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
 	var deltaTime = getDeltaTime();
-	player.update(deltaTime);
-	player.draw();
-	// context.drawImage(chuckNorris, SCREEN_WIDTH/2 - chuckNorris.width/2, SCREEN_HEIGHT/2 - chuckNorris.height/2);
 	
+	switch(gameState)
+	{
+		case STATE_PLAY:
+			updateStatePlay(deltaTime);
+			drawStatePlay();
+			break;
+		case STATE_GAME_OVER:
+			updateStateGameOver(deltaTime);
+			drawStateGameOver();
+			break;
+	}
+				
 		
-	// update the frame counter 
+		// update the frame counter 
 	fpsTime += deltaTime;
 	fpsCount++;
 	if(fpsTime >= 1)
@@ -72,6 +482,8 @@ function run()
 	context.font="14px Arial";
 	context.fillText("FPS: " + fps, 5, 20, 100);
 }
+
+initialize();
 
 
 //-------------------- Don't modify anything below here
--- player.js
+++ player.js
@@ -1,34 +1,403 @@
-var Player = function() {
-	this.image = document.createElement("img");
-	this.x = canvas.width/2;
-	this.y = canvas.height/2;
+
+var LEFT = 0;
+var RIGHT = 1;
+
+var ANIM_IDLE_LEFT = 0;
+var ANIM_JUMP_LEFT = 1;
+var ANIM_WALK_LEFT = 2;
+var ANIM_SHOOT_LEFT = 3;
+var ANIM_CLIMB = 4;
+var ANIM_IDLE_RIGHT = 5;
+var ANIM_JUMP_RIGHT = 6;
+var ANIM_WALK_RIGHT = 7;
+var ANIM_SHOOT_RIGHT = 8;
+var ANIM_MAX = 9;
+
+var STATE_RUN_JUMP = 0;
+var STATE_CLIMB = 1;
+
+var Player = function() {	
+	this.sprite = new Sprite("ChuckNorris.png");
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05, [0, 1, 2, 3, 4, 5, 6, 7]);
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05, [8, 9, 10, 11, 12]);
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05, [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]);
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05, [27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]);	
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05, [41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51]);
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05, [52, 53, 54, 55, 56, 57, 58, 59]);
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05, [60, 61, 62, 63, 64]);
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05, [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78]);
+	this.sprite.buildAnimation(12, 8, 165, 126, 0.05, [79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92]);	
+	for(var i=0; i<ANIM_MAX; i++)
+	{
+		this.sprite.setAnimationOffset(i, -55, -87);
+	}
+	
+	this.position = new Vector2();
+	this.position.set(9*TILE, 0*TILE); 	
 	this.width = 159;
 	this.height = 163;
 	
-	this.image.src = "hero.png";
+
+	this.velocity = new Vector2();
+	this.falling = true;
+	this.jumping = false;
+	this.isAlive = true;
+	this.bulletTimer = 0;	
+	
+	this.direction = LEFT;
+	this.state = STATE_RUN_JUMP;
+   
 };
 
-Player.prototype.update = function(deltaTime)
+Player.prototype.respawn = function()
 {
-	if(typeof(this.rotation) == "undefined")
-		this.rotation = 0;
-		
-	if(keyboard.isKeyDown(keyboard.KEY_SPACE) == true)
+	this.position.set(9*TILE, 0*TILE);
+	this.falling = true;
+	this.jumping = false;
+	this.isAlive = true;
+}
+
+Player.prototype.updateClimbState = function(dt)
+{
+	var left = false;
+	var right = false;
+	var climbUp = false;
+	var climbDown = false;
+	
+	if(keyboard.isKeyDown(keyboard.KEY_UP) == true)
 	{
-		this.rotation -= deltaTime;
+		climbUp = true;
+		this.sprite.update(dt);
 	}
-	else
+	if(keyboard.isKeyDown(keyboard.KEY_DOWN) == true)
 	{
-		this.rotation += deltaTime;
+		climbDown = true;
+		this.sprite.update(dt);
+	}
+	
+	var wasleft = this.velocity.x < 0;
+	var wasright = this.velocity.x > 0;
+	var wasup = this.velocity.y < 0;
+	var wasdown = this.velocity.y > 0;
+	
+	//acceleration
+	var ddx = 0;
+	var ddy = 0;
+	
+	if(left)
+		ddx = ddx - ACCEL; // wants to go left
+	else if (wasleft)
+		ddx = ddx + FRICTION; // was going left, not now
+	
+	if (right)
+		ddx = ddx + ACCEL;     // player wants to go right
+	else if (wasright)
+		ddx = ddx - FRICTION;  // player was going right, but not any more
+	
+	if(climbUp)
+		ddy = ddy - ACCEL; // wants to go up
+	else if(wasup)
+	{
+		this.velocity.y = 0;
+		ddx = 0;
+	}
+	
+	if(climbDown)
+		ddy = ddy + ACCEL; // wants to go down
+	else if(wasdown)
+	{
+		this.velocity.y = 0;
+		ddx = 0;
+	}
+	
+	// And integrate the forces to calculate the new position and velocity:
+	this.position.y = Math.floor(this.position.y  + (dt * this.velocity.y));
+	this.position.x = Math.floor(this.position.x  + (dt * this.velocity.x));
+	this.velocity.x = bound(this.velocity.x + (dt * ddx), -MAXDX, MAXDX);
+	this.velocity.y = bound(this.velocity.y + (dt * ddy), -MAXDY, MAXDY);
+	
+	
+	if ((wasleft  && (this.velocity.x > 0)) ||
+		(wasright && (this.velocity.x < 0))) 
+	{		
+		// clamp at zero to prevent friction from making us jiggle side to side
+		this.velocity.x = 0; 
+	}
+	
+	// if we go past the end of the ladder (either top or bottom) then 
+	// switch back to the RunJump state
+	var tx = pixelToTile(this.position.x);
+	var ty = pixelToTile(this.position.y);
+    var nx = (this.position.x)%TILE;         // true if player overlaps right
+    var ny = (this.position.y)%TILE;         // true if player overlaps below
+    var cell = cellAtTileCoord(LAYER_LADDERS, tx, ty);
+    var cellright = cellAtTileCoord(LAYER_LADDERS, tx + 1, ty);
+    var celldown = cellAtTileCoord(LAYER_LADDERS, tx, ty + 1);
+    var celldiag = cellAtTileCoord(LAYER_LADDERS, tx + 1, ty + 1);
+	
+	if (this.velocity.y > 0 || wasdown) 		// moving down
+	{
+		if ((!celldown && cell) || (!celldiag && cellright && nx)) 
+		{
+			this.position.y = tileToPixel(ty);      // clamp the y position to avoid falling into platform below
+			this.velocity.y = 0;            // stop downward velocity						
+			this.state = STATE_RUN_JUMP;
+		}
+	}
+	else if (this.velocity.y < 0 || wasup) 	// moving up
+	{
+		if ((!cell && !celldown) && ((!cellright && !celldiag && nx) || !nx))
+		{
+			this.position.y = tileToPixel(ty + 1);  // clamp the y position to avoid jumping into platform above
+			this.velocity.y = 0;            // stop upward velocity				
+			this.state = STATE_RUN_JUMP;
+		}		
+	}		
+		
+};
+
+Player.prototype.updateRunJumpState = function(deltaTime)
+{
+	var left = false;
+	var right = false;
+	var jump = false;
+	
+	this.sprite.update(deltaTime);
+
+	// check keypress events
+	if(keyboard.isKeyDown(keyboard.KEY_LEFT) == true) {
+		left = true;
+		this.direction = LEFT;
+		if(this.sprite.currentAnimation != ANIM_WALK_LEFT && this.jumping == false)
+			this.sprite.setAnimation(ANIM_WALK_LEFT);
+	}
+	else if(keyboard.isKeyDown(keyboard.KEY_RIGHT) == true) {
+		right = true;
+		this.direction = RIGHT;
+		if(this.sprite.currentAnimation != ANIM_WALK_RIGHT && this.jumping == false)
+			this.sprite.setAnimation(ANIM_WALK_RIGHT);
+	}
+	else {
+		if(this.jumping == false && this.falling == false)
+		{
+			if(this.direction == LEFT) 
+			{
+				if(this.sprite.currentAnimation != ANIM_IDLE_LEFT)
+					this.sprite.setAnimation(ANIM_IDLE_LEFT);
+			}
+			else
+			{
+				if(this.sprite.currentAnimation != ANIM_IDLE_RIGHT)
+					this.sprite.setAnimation(ANIM_IDLE_RIGHT);
+			}
+		}	
+		
+	}	
+	
+	if(keyboard.isKeyDown(keyboard.KEY_UP) == true) {
+		jump = true;
+		if(left == true){
+			this.sprite.setAnimation(ANIM_JUMP_LEFT);
+		}
+		if(right == true){
+			this.sprite.setAnimation(ANIM_JUMP_RIGHT);
+		}
+	}
+	
+	if(this.bulletTimer > 0)
+	{
+		this.bulletTimer -= deltaTime;
+	}
+	if(keyboard.isKeyDown(keyboard.KEY_SPACE)==true && this.bulletTimer<=0){
+		sfxFire.play();
+		this.bulletTimer = 0.3;
+		// Fire bullet code
+
+		var bullet = new Bullet(this.position.x, this.position.y, (this.direction == RIGHT));
+		bullets.push(bullet);
+		
+	}
+	
+	
+	var wasleft = this.velocity.x < 0;
+	var wasright = this.velocity.x > 0;
+	var falling = this.falling;
+	var ddx = 0;
+	var ddy = GRAVITY;	
+	
+	if(left)
+		ddx -= ACCEL; // player wants to go left
+	else if(wasleft)
+		ddx += FRICTION // player was going, dosen't want to anymore
+
+	if(right)
+		ddx += ACCEL; // player wants to go right
+	else if(wasright)
+		ddx -= FRICTION // player was going, dosen't want to anymore
+	
+	if(jump && !this.jumping && !falling)
+	{
+		ddy -= JUMP; // appy instantaneous jump
+		this.jumping = true;
+		if(this.direction == LEFT)
+			this.sprite.setAnimation(ANIM_JUMP_LEFT)
+		else
+			this.sprite.setAnimation(ANIM_JUMP_RIGHT)	
+	}
+		
+
+ 
+	// calculate the new position and velocity:	
+	this.position.y = Math.floor(this.position.y  + (deltaTime * this.velocity.y));
+	this.position.x = Math.floor(this.position.x  + (deltaTime * this.velocity.x));
+	this.velocity.x = bound(this.velocity.x + (deltaTime * ddx), -MAXDX, MAXDX);
+	
+	this.velocity.y = bound(this.velocity.y + (deltaTime * ddy), -MAXDY, MAXDY);
+	
+	if((wasleft && (this.velocity.x >0)) ||
+		(wasright && (this.velocity.x < 0)))
+	{
+		// clamp at zero to prevent friction rom making us jiggle from side-side
+
+		this.velocity.x = 0;
+	}
+	
+	// collision detection
+	// Our collision detection logic is greatly simplified by the fact that the player is a rectangle 
+	// and is exactly the same size as a single tile. So we know that the player can only ever 
+	// occupy 1, 2 or 4 cells.
+	
+	// This means we can short-circuit and avoid building a general purpose collision detection 
+	// engine by simply looking at the 1 to 4 cells that the player occupies:
+	var tx = pixelToTile(this.position.x);
+	var ty = pixelToTile(this.position.y);
+    var nx = (this.position.x)%TILE;         // true if player overlaps right
+    var ny = (this.position.y)%TILE;         // true if player overlaps below
+    var cell = cellAtTileCoord(LAYER_PLATFORMS, tx, ty);
+    var cellright = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty);
+    var celldown = cellAtTileCoord(LAYER_PLATFORMS, tx, ty + 1);
+    var celldiag = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty + 1);
+    
+	// If the player has vertical velocity, then check to see if they have hit a platform below 
+	// or above, in which case, stop their vertical velocity, and clamp their y position:	
+	
+	if (this.velocity.y>0)
+	{
+		if((celldown && !cell) || (celldiag && !cellright && nx))
+		{
+			this.position.y = tileToPixel(ty); // clamp y position to avoid falling into platform below
+			this.velocity.y = 0; // stop downward velocity
+			this.falling = false; // no longer falling
+			this.jumping = false; // no longer jumping
+			ny = 0; //no longer overlaps the cell below
+		}
+	}    
+    else if(this.velocity.y <0)
+    {
+	    if((cell & !celldown) || (cellright && !celldiag && nx))
+	    {
+		    this.position.y = tileToPixel(ty+1); // clamp the y position to avoid jumping into platform above
+		    this.velocity.y = 0; // stop upward velocity
+		    cell = celldown; // player in no longer in that cell so we clamp them to the cell below
+		    cellright = celldiag; // as above
+		    ny = 0; // player no longer overlapping cells 
+	    }
+    }
+	// Apply similar logic to the horizontal velocity:
+	if (this.velocity.x > 0) 
+	{
+		if ((cellright && !cell) || (celldiag  && !celldown && ny)) 
+		{
+			this.position.x = tileToPixel(tx); 		// clamp the x position to avoid moving into the platform we just hit
+			this.velocity.x = 0;			// stop horizontal velocity
+		}
+	}
+	else if (this.velocity.x < 0) 
+	{
+		if ((cell && !cellright) || (celldown && !celldiag && ny)) 
+		{
+			this.position.x = tileToPixel(tx + 1);	// clamp the x position to avoid moving into the platform we just hit
+			this.velocity.x = 0;          	// stop horizontal velocity
+		}
+	}	
+	
+	// The last calculation for our update() method is to detect if the player is now falling or 
+	// not. We can do that by looking to see if there is a platform below them
+	this.falling = ! (celldown || (nx && celldiag)); 
+	
+	/*check if the character is standing near a ladder. If they are and the player presses either up or down (depending on whether they 	are standing at the top or bottom of the ladder) then switch to the CLIMB state The player must be standing still and not falling or 	jumping in order to climb a ladder
+	*/
+	if(right == false && left == false && this.falling == false) 
+	{
+		var ladderCell = cellAtTileCoord(LAYER_LADDERS, tx, ty);
+		var ladderCellright = cellAtTileCoord(LAYER_LADDERS, tx + 1, ty);	
+		var ladderCelldown = cellAtTileCoord(LAYER_LADDERS, tx, ty + 1);
+		var ladderCelldiag = cellAtTileCoord(LAYER_LADDERS, tx + 1, ty + 1);
+		
+		if(ladderCell || (ladderCellright && nx))
+		{
+			if(keyboard.isKeyDown(keyboard.KEY_UP) == true)
+			{
+				this.state = STATE_CLIMB;
+				this.sprite.setAnimation(ANIM_CLIMB);
+				return;
+			}
+		}
+		
+		if(ladderCelldown || (ladderCelldiag && nx))
+		{
+			if(keyboard.isKeyDown(keyboard.KEY_DOWN) == true)
+			{
+				this.state = STATE_CLIMB;
+				this.sprite.setAnimation(ANIM_CLIMB);
+				return;
+			}
+		}
 	}
 	
 }
 
+
+
+
+Player.prototype.update = function(deltaTime)
+{		
+	if(this.isAlive == false)
+		return;
+		
+	if(gameState == STATE_GAME_OVER)
+	{
+		if(this.sprite.currentAnimation != ANIM_IDLE_RIGHT)
+			this.sprite.setAnimation(ANIM_IDLE_RIGHT);
+		this.sprite.update(deltaTime);
+		return;
+	}
+	if(this.state == STATE_RUN_JUMP)
+	{
+		this.updateRunJumpState(deltaTime);
+	}
+	else if(this.state == STATE_CLIMB)
+	{
+		this.updateClimbState(deltaTime);
+	}
+	/*
+	// check to see if player has reached the end of the game	
+	var tx = pixelToTile(this.position.x);
+	var ty = pixelToTile(this.position.y);
+	if(cellAtTileCoord(LAYER_TRIGGERS, tx, ty) !=0)
+	{
+		gameState = STATE_GAME_OVER;
+	}			
+	*/      
+}
+
 Player.prototype.draw = function()
 {
-	context.save();
-		context.translate(this.x, this.y);
-		context.rotate(this.rotation);
-		context.drawImage(this.image, -this.width/2, -this.height/2);
-	context.restore();
+
+	if(this.isAlive == false)
+		return;
+	
+	var screenX = this.position.x - worldOffsetX;			
+	this.sprite.draw(context, screenX, this.position.y);
+	
 }
